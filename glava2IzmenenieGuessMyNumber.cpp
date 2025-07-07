315be697afbc5fce4bc943bcc4c7b211804eb103
Михаил Рулев <52673140+vld7wn@users.noreply.github.com>
2025-07-07 23:06:26 +0300

    Create glava2IzmenenieGuessMyNumber.cpp
    Немного перепафаное угадай число

---
+++ glava2IzmenenieGuessMyNumber.cpp
@@ -0,0 +1,70 @@
+//игра по книге "Угадай число"
+/*
+Было добавленно:
+  Теперь компьютер так должен угадать число
+  Возможность сразу повторно сыграть или закончить игру
+*/
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <utility>
+#include <vector>
+//Задаем директивы using для того, чтобы не писать std::...
+using std::cout;
+using std::endl;
+using std::cin;
+using std::string;
+
+int main() {
+    std::srand(std::time(0));
+    int randomNumber;
+    int choice = 0;
+    int numberPlayer;
+
+    do {
+        cout << "Choice ho search number: \n";
+        cout << "1 - Player\n";
+        cout << "2 - Computer\n\n";
+        cout << "Choice ho play: ";
+        cin >> choice;
+        if (choice == 1) {
+            randomNumber = rand() % 100 + 1;
+            do {
+                ++choice;
+                cout << "Enter number: ";
+                cin >> numberPlayer;
+
+                if (numberPlayer > randomNumber) {
+                    cout << "Small!\n";
+                } else if (numberPlayer < randomNumber) {
+                    cout << "More!\n";
+                }
+
+            } while (numberPlayer != randomNumber);
+            cout << "Random number equals for your: " << numberPlayer << " Count search your number: " << choice << endl;
+
+        } else if (choice == 2) {
+            cout << "Enter number: ";
+            cin >> numberPlayer;
+            do {
+                ++choice;
+                randomNumber = rand() % 100 + 1;
+            } while (randomNumber != numberPlayer);
+            cout << "Your number: " << randomNumber << " Count search your number: " << choice << endl;
+        }
+
+        cout << "Your play again:\n ";
+        cout << "Press enter to  '1 - continue' : '2 - break': ";
+        cin >> numberPlayer;
+
+        if (numberPlayer == 1) {
+            continue;
+        } else if (numberPlayer == 2) {
+            break;
+        }
+
+    } while (numberPlayer != randomNumber || randomNumber != numberPlayer);
+
+    return 0;
+}
